7a233d75b6a34b8f20ff9e0371d8e6e5efe6bb7d
Elizar Pepino <jupenz@gmail.com>
2014-08-14 18:18:02 +0800

    Initial Commit

---
+++ todo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  todo
+//
+//  Created by Elizar Pepino on 8/14/14.
+//  Copyright (c) 2014 Elizar Pepino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
